26b95715e2e23e3aeb98280d3d106411ad6b150b
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2021-02-09 18:17:09 -0500

    test message

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // and set the environment variables. See http://twil.io/secure
+require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
-require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
-client.messages
-  .create({
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-    from: process.env.TWILIO_PHONE_NUM,
-    to: process.env.TO_PHONE_NUM,
-  })
-  .then((message) => console.log(message.sid));
+const app = express();
+
+app.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message("The Robots are coming! Head for the hills!");
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log("Express server listening on port 1337");
+});
