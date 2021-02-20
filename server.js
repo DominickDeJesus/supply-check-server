cfa6f988b45bf5675ed394a3a28c51fa3d708f3f
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2021-02-20 14:22:03 -0500

    supply checker not finished initializing

--- server.js
+++ server.js
@@ -26,27 +26,38 @@ app.use(express.urlencoded({ extended: false }));
 app.post("/sms", async (req, res) => {
   const twiml = new MessagingResponse();
   const textResponse = req.body.Body.toLowerCase().trim();
-  console.log(req.body.Body);
-  switch (textResponse) {
-    case "status":
-      twiml.message(
-        `Server is running! Browser initialized: ${sc.finishedInit}`
-      );
-      break;
-    case "last":
-      twiml.message(`Last in stock on: ${sc.lastMessageDate || "Unknown."}`);
-      break;
-    case "refresh":
-      const inStock = await sc.checkStock();
-      twiml.message(
-        `Checking status now... ${inStock ? "in stock!!!" : "not in stock."} `
-      );
-      break;
-    default:
-      twiml.message(
-        'Type: \n"status" - server status\n"last" - last date in stock\n"refresh" - force refresh of page'
-      );
-      break;
+  try {
+    switch (textResponse) {
+      case "status":
+        twiml.message(
+          `Server is running! Browser initialized: ${sc.finishedInit}`
+        );
+        break;
+      case "last":
+        twiml.message(`Last in stock on: ${sc.lastMessageDate || "Unknown."}`);
+        break;
+      case "refresh":
+        const inStock = await sc.checkStock();
+        twiml.message(
+          `Checking status now... ${inStock ? "in stock!!!" : "not in stock."} `
+        );
+        break;
+      case "init":
+        const inStock = await sc.checkStock();
+        twiml.message(
+          `Checking status now... ${inStock ? "in stock!!!" : "not in stock."} `
+        );
+        break;
+      default:
+        twiml.message(
+          'Type: \n"status" - server status\n"last" - last date in stock\n"refresh" - force refresh of page'
+        );
+        break;
+    }
+  } catch (error) {
+    if (error.message === "SupplyChecker has not been initialized!") {
+      twiml.message(`SupplyChecker has not finished initializing...`);
+    } else console.log(error.message);
   }
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
