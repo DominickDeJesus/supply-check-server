935bad850540bdee4f93089d2880a3c7754e8999
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2021-03-01 20:51:21 -0500

    added content of html in unkown case

--- supplyChecker.js
+++ supplyChecker.js
@@ -3,126 +3,126 @@ const puppeteer = require("puppeteer");
 const { getTimestamp, isToday } = require("./utils");
 
 class SupplyChecker {
-  constructor(url) {
-    this.finishedInit = false;
-    this.url = url;
-    this.lastMessageDate = null;
-    this.tag = `button[data-sku-id="${url.split("skuId=")[1]}"]`;
-    this.browserOption =
-      process.platform === "linux"
-        ? {
-            args: ["--no-sandbox"],
-          }
-        : null;
-  }
-  async init() {
-    console.log(getTimestamp(), " Initializing browser");
+	constructor(url) {
+		this.finishedInit = false;
+		this.url = url;
+		this.lastMessageDate = null;
+		this.tag = `button[data-sku-id="${url.split("skuId=")[1]}"]`;
+		this.browserOption =
+			process.platform === "linux"
+				? {
+						args: ["--no-sandbox"],
+				  }
+				: null;
+	}
+	async init() {
+		console.log(getTimestamp(), " Initializing browser");
 
-    this.browser = await puppeteer.launch({
-      headless: true,
-      ...this.browserOption,
-    });
+		this.browser = await puppeteer.launch({
+			headless: true,
+			...this.browserOption,
+		});
 
-    this.page = await this.browser.newPage();
-    await this.page.setDefaultNavigationTimeout(0);
+		this.page = await this.browser.newPage();
+		await this.page.setDefaultNavigationTimeout(0);
 
-    await this.page.goto(this.url, {
-      waitUntil: "load",
-    });
-    this.finishedInit = true;
-    console.log(getTimestamp(), " Finished initializing");
-  }
+		await this.page.goto(this.url, {
+			waitUntil: "load",
+		});
+		this.finishedInit = true;
+		console.log(getTimestamp(), " Finished initializing");
+	}
 
-  async checkStock() {
-    if (!this.finishedInit)
-      throw new Error("SupplyChecker has not been initialized!");
+	async checkStock() {
+		if (!this.finishedInit)
+			throw new Error("SupplyChecker has not been initialized!");
 
-    await this.page.reload();
-    if (
-      (await this.isInStock(this.page, this.tag)) &&
-      !isToday(this.lastMessageDate)
-    ) {
-      this.sendTextNotification(url);
-      lastMessageDate = new Date();
-      await this.screenshot();
-      return true;
-    }
-    return false;
-  }
+		await this.page.reload();
+		if (
+			(await this.isInStock(this.page, this.tag)) &&
+			!isToday(this.lastMessageDate)
+		) {
+			this.sendTextNotification(url);
+			lastMessageDate = new Date();
+			await this.screenshot();
+			return true;
+		}
+		return false;
+	}
 
-  async isInStock(page, tag) {
-    if (!this.finishedInit)
-      throw new Error("SupplyChecker has not been initialized!");
-    const $ = require("cheerio");
-    try {
-      console.log(getTimestamp(), " Loading page content");
-      const html = await page.content();
-      const buttonText = $(tag, html).text();
+	async isInStock(page, tag) {
+		if (!this.finishedInit)
+			throw new Error("SupplyChecker has not been initialized!");
+		const $ = require("cheerio");
+		try {
+			console.log(getTimestamp(), " Loading page content");
+			const html = await page.content();
+			const buttonText = $(tag, html).text();
 
-      if (buttonText.toLocaleLowerCase() === "sold out") {
-        console.log(
-          getTimestamp(),
-          ` Out of stock! Tag content: ${buttonText}`
-        );
-        return false;
-      } else if (buttonText.toLocaleLowerCase().includes("add")) {
-        console.log(getTimestamp(), " In stock!!! Tag content: ", buttonText);
-        return true;
-      } else {
-        console.log(
-          getTimestamp(),
-          " Button content unkown! Tag content: ",
-          buttonText
-        );
-        return false;
-      }
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  }
+			if (buttonText.toLocaleLowerCase() === "sold out") {
+				console.log(
+					getTimestamp(),
+					` Out of stock! Tag content: ${buttonText}`
+				);
+				return false;
+			} else if (buttonText.toLocaleLowerCase().includes("add")) {
+				console.log(getTimestamp(), " In stock!!! Tag content: ", buttonText);
+				return true;
+			} else {
+				console.log(
+					getTimestamp(),
+					" Button content unknown! Tag html content: ",
+					`${$(tag, html).html()}`
+				);
+				return false;
+			}
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	}
 
-  async changeUrl(url) {
-    this.url = url;
-    this.lastMessageDate = null;
-    await this.page.goto(this.url, {
-      waitUntil: "load",
-    });
-  }
+	async changeUrl(url) {
+		this.url = url;
+		this.lastMessageDate = null;
+		await this.page.goto(this.url, {
+			waitUntil: "load",
+		});
+	}
 
-  async screenshot() {
-    if (!this.finishedInit)
-      throw new Error("SupplyChecker has not been initialized!");
+	async screenshot() {
+		if (!this.finishedInit)
+			throw new Error("SupplyChecker has not been initialized!");
 
-    await this.page.screenshot({
-      path: `./screenshots/${this.lastMessageDate}-screenshot.png`,
-      fullPage: true,
-    });
-  }
-  async sendTextNotification(url) {
-    if (!this.finishedInit)
-      throw new Error("SupplyChecker has not been initialized!");
-    try {
-      const client = require("twilio")(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_AUTH_TOKEN
-      );
+		await this.page.screenshot({
+			path: `./screenshots/${this.lastMessageDate}-screenshot.png`,
+			fullPage: true,
+		});
+	}
+	async sendTextNotification(url) {
+		if (!this.finishedInit)
+			throw new Error("SupplyChecker has not been initialized!");
+		try {
+			const client = require("twilio")(
+				process.env.TWILIO_ACCOUNT_SID,
+				process.env.TWILIO_AUTH_TOKEN
+			);
 
-      const message = await client.messages.create({
-        body: `In stock alert!!! \n\n${url}`,
-        from: process.env.FROM_PHONE,
-        to: process.env.TO_PHONE,
-      });
+			const message = await client.messages.create({
+				body: `In stock alert!!! \n\n${url}`,
+				from: process.env.FROM_PHONE,
+				to: process.env.TO_PHONE,
+			});
 
-      console.log(getTimestamp(), " Message sent! ", message.sid);
-    } catch (error) {
-      console.log(
-        getTimestamp(),
-        "Something went wrong, message was not sent\n",
-        error
-      );
-    }
-  }
+			console.log(getTimestamp(), " Message sent! ", message.sid);
+		} catch (error) {
+			console.log(
+				getTimestamp(),
+				"Something went wrong, message was not sent\n",
+				error
+			);
+		}
+	}
 }
 
 module.exports = SupplyChecker;
